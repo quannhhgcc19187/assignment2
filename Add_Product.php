1b8db4ce714e2c5fda4eef417c8da9f14b69174b
quannhhgcc19187 <91667696+quannhhgcc19187@users.noreply.github.com>
2021-11-18 01:21:19 +0700

    Update Add_Product.php

--- Add_Product.php
+++ Add_Product.php
@@ -1,196 +1,164 @@
-<link rel="stylesheet" href="css/bootstrap.min.css">
-<script type="text/javascript" src="scripts/ckeditor/ckeditor.js"></script>
-<?php
-	require('requirelogin.php');
+    <?php
 	include_once("connection.php");
-	function bind_Category_List($conn) {
-		$sqlstring = "SELECT cat_id, cat_name from category";
-		$result = pg_query($conn, $sqlstring);
-		echo "<SELECT name='CategoryList' class='form-control'>
+	
+	function bind_Category_List($conn){
+		$sqlstring ="SELECT cat_id, cat_name from category";
+		$result= pg_query($conn, $sqlstring);
+		echo"<SELECT name ='CategoryList'class='form-control '
 			<option value='0'>Choose category</option>";
-			while ($row = pg_fetch_array($result, NULL, PGSQL_ASSOC)) {
-				echo "<option value='".$row['cat_id']."'>".$row['cat_name']."</option>";
+			while($row = pg_fetch_array($result, NULL, PGSQL_ASSOC)){
+				echo"<OPTION value='".$row['cat_id']."'>".$row['cat_name']. "</option>";
 			}
-		echo "</select>";	
+			echo"</select>";
+	}
+	function bind_Shop_List($conn){
+		$sqlstring ="SELECT shop_id, shop_name from shop";
+		$result= pg_query($conn, $sqlstring);
+		echo"<SELECT name ='BranchList'class='form-control '
+			<option value='0'>Choose branch</option>";
+			while($row = pg_fetch_array($result, NULL, PGSQL_ASSOC)){
+				echo"<OPTION value='".$row['shop_id']."'>".$row['shop_name']. "</option>";
+			}
+			echo"</select>";
+	}
+
+	if(isset($_POST["btnAdd"]))
+	{  
+		$id = $_POST["txtID"];
+		$proname=$_POST["txtName"];
+		$short=$_POST['txtShort'];
+		$branch=$_POST['txtbranch'];
+		$detail=$_POST['txtDetail'];
+		$price=$_POST['txtPrice'];
+		$qty=$_POST['txtQty'];
+        $pic=$_FILES['txtImage'];
+        $category=$_POST['CategoryList'];
+		$shop=$_POST['BranchList'];
+		
+		$err="";
+		
+		if(trim($proname)==""){
+			$err.="<li>Enter product name, please</li>";
 		}
-
-		function bind_Shop_List($conn) {
-			$sqlstring = "SELECT shop_id, shop_name from public.store";
-			$result = pg_query($conn, $sqlstring);
-			echo "<SELECT name='StoreList' class='form-control'>
-				<option value='0'>Choose store</option>";
-				while ($row = pg_fetch_array($result, NULL, PGSQL_ASSOC)) {
-					echo "<option value='".$row['shop_id']."'>".$row['shop_name']."</option>";
-				}
-			echo "</select>";	
-			}
-
-		if(isset($_POST["btnAdd"]))
-		{
-			$id = $_POST["txtID"];
-			$id = htmlspecialchars(pg_escape_string($conn, $id));
-		    $proname = $_POST["txtName"];
-			$proname = htmlspecialchars(pg_escape_string($conn, $proname));
-		    $short = $_POST["txtShort"];
-			$short = htmlspecialchars(pg_escape_string($conn, $short));
-		    $detail = $_POST['txtDetail'];
-			$detail = htmlspecialchars(pg_escape_string($conn, $detail));
-		    $price = $_POST['txtPrice'];
-			$price = htmlspecialchars(pg_escape_string($conn, $price));
-		    $qty = $_POST['txtQty'];
-			$qty = htmlspecialchars(pg_escape_string($conn, $qty));
-	    	$pic = $_FILES['txtImage'];
-		    $category = $_POST['CategoryList'];
-			$category = htmlspecialchars(pg_escape_string($conn, $category));
-			$shop = $_POST['StoreList'];
-			$shop = htmlspecialchars(pg_escape_string($conn, $shop));
-		    $err = "";
-
-			if(trim($id)==""){
-				$err .="<li>Enter product ID, please</li>";
-			}
-			if(trim($proname)==""){
-			   $err .="<li>Enter product name, please</li>";
-		    }
-		    if($category=="0"){
-			   $err .="<li>Choose product category, please</li>";
-		    }
-			if($shop=="0"){
-				$err .="<li>Choose shop, please</li>";
-			 }
-		    if(!is_numeric($price)){
-			   $err .="<li>Product price must be number, please</li>";
-		    }
-		    if(!is_numeric($qty)){
-			   $err .="<li>Product quantiy must be number, please</li>";
-		    }
-		    if($err !=""){
-			   echo "<ul>$err</ul>";
-		   }
-			
-			else
-			{
-				if($pic['type']=="image/jpg" || $pic['type']=="image/jpeg" || $pic['type']=="image/png"|| $pic['type']=="image/gif")
-				{
-					if ($pic['size'] <= 614400)
+		if(!is_numeric($price)){
+			$err.="<li>Product price must be number</li>";
+		}
+		if(!is_numeric($qty)){
+			$err.="<li>Product quantity must be number</li>";
+		}
+		if($err !=""){
+			echo"<ul>$err</ul>";
+		}
+		else{
+			if($pic['type']=="image/jpg"||$pic['type']=="image/jpeg"||$pic['type']=="image/png" ||$pic['type']=="image/gif"){
+				if($pic['size']<=614400){
+					$sq="SELECT * from product where product_id='$id'or product_name='$proname'";
+                    $result= pg_query($conn,$sq);
+                    
+					if(pg_num_rows($result)==0)
 					{
-						$sq = "SELECT * FROM public.product WHERE product_id='$id' OR pro_name='$proname'";
-						$result = pg_query($conn, $sq);
-						
-						if(pg_num_rows($result)==0)
-						{
-							copy($pic['tmp_name'], "product_image/".$pic['name']);
-							$filePic = $pic['name'];
-							$sqlstring = "INSERT INTO product(
-								product_id, product_Name, price, smalldesc, detaildesc, prodate, pro_qty, pro_image, cat_id, shop_id)
-								VALUES ('$id', '$proname', '$price', '$short', '$detail', '".date('Y-m-d H:i:s')."', '$qty', '$filePic', '$category,'$shop')";
-							pg_query($conn, $sqlstring);
-							echo '<meta http-equiv="refresh" content="0;URL=index.php?page=product"/>';
-						}
-						else
-						{
-							echo "<script>alert('Duplicate product ID')</script>";
-						}
+						copy($pic['tmp_name'],"ATNtoy/".$pic['name']);
+						$filePic =$pic['name'];
+						$sqlstring="INSERT INTO product(
+							product_id, product_name, price, smalldesc, detaildesc, prodate, pro_qty, pro_image, cat_id, shop_id)
+							VALUES('$id','$proname', $price,'$short','$detail','".date('Y-m-d H:i:s')."',$qty,'$filePic','$category', '$shop')";
+							
+						pg_query($conn, $sqlstring);
+						echo'<li>You have add successfully</li>';
 
+					}	
+					else {
+						echo"<li>Duplicate product ID or Name</li>";
 					}
-					else
-					{
-						echo "<script>alert('Size of the image too big')</script>";
-						
-					}
-
 				}
-				else{
-					echo "<script>alert('Image format is not correct')</script>";
-				}
-			}
+            }
+            else
+            {
+                echo "Image fail";
+            }
+		}
 	}
 ?>
+   
 <div class="container">
-	<h2>Adding new Product</h2>
+	<h2>Adding Product</h2>
 
 	 	<form id="frmProduct" name="frmProduct" method="post" enctype="multipart/form-data" action="" class="form-horizontal" role="form">
+         
+         <form id="frmProduct" name="frmProduct" method="post" enctype="multipart/form-data" action="" class="form-horizontal" role="form">
 				<div class="form-group">
 					<label for="txtTen" class="col-sm-2 control-label">Product ID(*):  </label>
 							<div class="col-sm-10">
-							      <input type="text" name="txtID" id="txtID" class="form-control" placeholder="Product ID" value=''/>
+							      <input type="text" name="txtID" id="txtID" class="form-control" placeholder="Product ID" value="<?php if(isset($id)) echo $id?>"/>
 							</div>
-				</div> 
-				<div class="form-group">
-                <label for="txtTen" class="col-sm-2 control-label">Product Name(*):  </label>
+                
+				<div class="form-group"> 
+					<label for="txtTen" class="col-sm-2 control-label">Product Name(*):  </label>
 							<div class="col-sm-10">
-							      <input type="text" name="txtName" id="txtName" class="form-control" placeholder="Product Name" value=''/>
+								  <input type="text" name="txtName" id="txtName" class="form-control" 
+								  placeholder="Product Name" value=''/>
 							</div>
                 </div>   
-                <div class="form-group">   
-                    <label for="" class="col-sm-2 control-label">Product category(*):  </label>
-							<div class="col-sm-10">
-							      <?php bind_Category_List($conn);  ?>
-							</div>
-                </div>
-				<div class="form-group">   
-                    <label for="" class="col-sm-2 control-label">Shop(*):  </label>
-							<div class="col-sm-10">
-							      <?php bind_Shop_List($conn);  ?>
-							</div>
-                </div>  
                           
                 <div class="form-group">  
                     <label for="lblGia" class="col-sm-2 control-label">Price(*):  </label>
 							<div class="col-sm-10">
-							      <input type="text" name="txtPrice" id="txtPrice" class="form-control" placeholder="Price" value=''/>
+							      <input type="text" name="txtPrice" id="txtPrice" class="form-control" placeholder="Price" value="<?php if(isset($price)) echo $price?>"/>
 							</div>
-                 </div>     
-                
-                            
-                <div class="form-group">  
-        	        <label for="lblDetail" class="col-sm-2 control-label">Detail description(*):  </label>
+                 </div>   
+
+                 <div class="form-group">   
+                    <label for="" class="col-sm-2 control-label">Product category(*):  </label>
 							<div class="col-sm-10">
-							      <textarea name="txtDetail" rows="4" class="ckeditor"></textarea>
-              					  <script language="javascript">
-                                        CKEDITOR.replace( 'txtDetail',
-                                        {
-                                            skin : 'kama',
-                                            extraPlugins : 'uicolor',
-                                            uiColor: '#eeeeee',
-                                            toolbar : [ ['Source','DocProps','-','Save','NewPage','Preview','-','Templates'],
-                                                ['Cut','Copy','Paste','PasteText','PasteWord','-','Print','SpellCheck'],
-                                                ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
-                                                ['Form','Checkbox','Radio','TextField','Textarea','Select','Button','ImageButton','HiddenField'],
-                                                ['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
-                                                ['OrderedList','UnorderedList','-','Outdent','Indent','Blockquote'],
-                                                ['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
-                                                ['Link','Unlink','Anchor', 'NumberedList','BulletedList','-','Outdent','Indent'],
-                                                ['Image','Flash','Table','Rule','Smiley','SpecialChar'],
-                                                ['Style','FontFormat','FontName','FontSize'],
-                                                ['TextColor','BGColor'],[ 'UIColor' ] ]
-                                        });
-										
-                                    </script> 
-                                  
+                            
+							      <?php bind_Category_List($conn); ?>
+							</div>
+                </div>  
+                            
+                <div class="form-group">   
+                    <label for="lblShort" class="col-sm-12 control-label">Short description(*):  </label>
+							<div class="col-sm-10">
+							      <input type="text" name="txtShort" id="txtShort" class="form-control" placeholder="Short description" value="<?php if(isset($short)) echo $short?>"/>
+							</div>
+                </div>
+
+				<div class="form-group">   
+                    <label for="" class="col-sm-2 control-label">Shop(*):  </label>
+							<div class="col-sm-10">
+                            
+							      <?php bind_ShopList($conn); ?>
+							</div>
+                </div> 
+				       
+                <div class="form-group">   
+                    <label for="lblDetail" class="col-sm-2 control-label">Detail Description(*):  </label>
+							<div class="col-sm-10">
+							      <textarea type="text" name="txtDetail" id="txtDetail" class="form-control" style="height: 150px" row="4" value=""></textarea>
 							</div>
                 </div>
                             
             	<div class="form-group">  
-                    <label for="lblSoLuong" class="col-sm-2 control-label">Quantity(*):  </label>
+                    <label for="lblQty" class="col-sm-2 control-label">Quantity(*):  </label>
 							<div class="col-sm-10">
-							      <input type="number" name="txtQty" id="txtQty" class="form-control" placeholder="Quantity" value=""/>
+							      <input type="number" name="txtQty" id="txtQty" class="form-control" placeholder="Quantity" value="<?php if(isset($qty)) echo $qty?>"/>
 							</div>
                 </div>
  
 				<div class="form-group">  
-	                <label for="sphinhanh" class="col-sm-2 control-label">Image(*):  </label>
+	                <label for="lblImage" class="col-sm-2 control-label">Image(*):  </label>
 							<div class="col-sm-10">
+							
 							      <input type="file" name="txtImage" id="txtImage" class="form-control" value=""/>
 							</div>
                 </div>
                         
 				<div class="form-group">
 						<div class="col-sm-offset-2 col-sm-10">
-						      <input type="submit"  class="btn btn-primary" name="btnAdd" id="btnAdd" value="Add new"/>
-                              <input type="button" class="btn btn-primary" name="btnIgnore"  id="btnIgnore" value="Ignore" onclick="window.location='?page=product_management'" />
+						      <input type="submit"  class="site-btn" name="btnAdd" id="btnAdd" value="Add" onclick="window.location='?page=pm'" />
+                              <input type="button" class="site-btn" name="btnIgnore"  id="btnIgnore" value="Ignore" onclick="window.location='?page=pm'" />
                               	
 						</div>
 				</div>
 			</form>
+			</div>
 </div>
