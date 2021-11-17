486393610ca3880066f97575b8743f80e1c98a12
quannhhgcc19187 <91667696+quannhhgcc19187@users.noreply.github.com>
2021-11-18 01:42:10 +0700

    Update Add_Shop.php

--- Add_Shop.php
+++ Add_Shop.php
@@ -1,57 +1,56 @@
-     <!-- Bootstrap --> 
-     <link rel="stylesheet" type="text/css" href="style.css"/>
-	<meta charset="utf-8" />
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	    
-	<?php
-        include_once("connection.php");
-		if(isset($_POST["btnAdd"]))
+<?php
+		include_once("connection.php");
+		if(isset($_POST['btnAdd']))
 		{
-			$id = $_POST["txtID"];
-			$name = $_POST["txtName"];
-            $address = $_POST["txtAddress"];
-			$phone = $_POST["txtPhone"];
-            $email = $_POST["txtEmail"];
-			$err="";
-			if(trim($id)==""){
-				$err .="<li>Enter Shop ID, please</li>";
-			}
-			else if(trim($name)==""){
-				$err .="<li>Enter Shop Name, please</li>";
-			}
-            else if($address==""){
-				$err .="<li>Enter Shop Address, please</li>";
-			}
-			if(!is_numeric($phone)){
-				$err .="<li>Phone must be number, please</li>";
-			}
-            else if(strim($email)==""){
-				$err .="<li>Enter Shop Email, please</li>";
-			}
-			if($err!="")
+			$id = $_POST['txtID'];
+			$name = $_POST['txtName'];
+			$Address = $_POST['txtAddress'];
+			$phone = $_POST['txtPhone'];
+			$email = $_POST['txtEmail'];
+			$err = "";
+			if($id=="")
 			{
-				echo "<ul>$err</ul>";
+				$err .= "Enter category ID</br>";
 			}
-			else{
-                $id = htmlspecialchars(pg_real_escape_string($conn, $id));
-				$name = htmlspecialchars(pg_real_escape_string($conn, $name));
-				$sq = "SELECT * from shop where shop_id='$id' or cat_name='$name'";
-				$result = pg_query($conn,$sq);
-				if(pg_num_rows($result)==0)
+			if($name=="")
+			{
+				$err .= "Enter category name</br>";
+			}
+			if($Address=="")
+			{
+				$err .= "Enter address/br>";
+			}
+			if(!iS_number($phone)="")
+			{
+				$err .= "Enter phone</br>";
+			}
+			if($email=="")
+			{
+				$err .= "Enter email</br>";
+			}
+			if($err != "")
+			{
+				echo $err;
+			}
+			else
+			{
+				$sql = "select * from shop where shop_id ='$id' and shop_name = '$name'";
+				$result = pg_query($conn, $sql);
+				if(pg_num_rows($result)=="0")
 				{
-					pg_query($conn, "INSERT INTO shop (cat_id, cat_name, address, phone, email	) VALUES ('$id','$name','$address','$phone','$email)");
-					echo '<meta http-equiv="refresh" content="0;URL=?page=shop_management" />';
+					pg_query($conn, "insert into shop (shop_id, shop_name, address, phone, email) values ('$id', '$name','$Address','$phone','$email')");
+					echo '<meta http-equiv="refresh" content="0;URL =?page=shop_management"';
 				}
 				else
 				{
-					echo "<li>Duplicate Shop ID or Name</li>";
+					echo "Data duplicated";
 				}
 			}
 		}
 	?>
 
 <div class="container">
-	<h2>Adding Shop</h2>
+	<h2>Adding Category</h2>
 			 	<form id="form1" name="form1" method="post" action="" class="form-horizontal" role="form">
 				 <div class="form-group">
 						    <label for="txtTen" class="col-sm-2 control-label">Shop ID(*):  </label>
@@ -62,31 +61,31 @@
 				 <div class="form-group">
 						    <label for="txtTen" class="col-sm-2 control-label">Shop Name(*):  </label>
 							<div class="col-sm-10">
-							      <input type="text" name="txtName" id="txtName" class="form-control" placeholder="Cateogry Name" value='<?php echo isset($_POST["txtName"])?($_POST["txtName"]):"";?>'>
+							      <input type="text" name="txtName" id="txtName" class="form-control" placeholder="Category Name" value='<?php echo isset($_POST["txtName"])?($_POST["txtName"]):"";?>'>
 							</div>
 					</div>
-                    <div class="form-group">
+					<div class="form-group">
 						    <label for="txtTen" class="col-sm-2 control-label">Address(*):  </label>
 							<div class="col-sm-10">
-							      <input type="text" name="txtName" id="txtName" class="form-control" placeholder="Shop Address" value='<?php echo isset($_POST["txtName"])?($_POST["txtName"]):"";?>'>
+							      <input type="text" name="txtAddress" id="txtAddress" class="form-control" placeholder="Address" value='<?php echo isset($_POST["txtAddress"])?($_POST["txtAddress"]):"";?>'>
 							</div>
 					</div>
                     <div class="form-group">
 						    <label for="txtTen" class="col-sm-2 control-label">Phone(*):  </label>
 							<div class="col-sm-10">
-							      <input type="text" name="txtName" id="txtName" class="form-control" placeholder="Shop Phone" value='<?php echo isset($_POST["txtName"])?($_POST["txtName"]):"";?>'>
+							      <input type="text" name="txtPhone" id="txtPhone" class="form-control" placeholder="Phone" value='<?php echo isset($_POST["txtPhone"])?($_POST["txtPhone"]):"";?>'>
 							</div>
 					</div>
-                    <div class="form-group">
+					<div class="form-group">
 						    <label for="txtTen" class="col-sm-2 control-label">Email(*):  </label>
 							<div class="col-sm-10">
-							      <input type="text" name="txtName" id="txtName" class="form-control" placeholder="Shop Email" value='<?php echo isset($_POST["txtName"])?($_POST["txtName"]):"";?>'>
+							      <input type="text" name="txtEmail" id="txtEmail" class="form-control" placeholder="Email" value='<?php echo isset($_POST["txtEmail"])?($_POST["txtEmail"]):"";?>'>
 							</div>
 					</div>
 					<div class="form-group">
 						<div class="col-sm-offset-2 col-sm-10">
-						      <input type="submit"  class="btn btn-primary" name="btnAdd" id="btnAdd" value="Add new"/>
-                              <input type="button" class="btn btn-primary" name="btnIgnore"  id="btnIgnore" value="Ignore" onclick="window.location='?page=shop_management'" />
+						      <input type="submit"  class="site-btn" name="btnAdd" id="btnAdd" value="Add new"/>
+                              <input type="button" class="site-btn" name="btnIgnore"  id="btnIgnore" value="Ignore" onclick="window.location='?page=category_management'" />
                               	
 						</div>
 					</div>
