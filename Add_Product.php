bca4a0d5bc4f9849d55bf1d9dedb8b9336d509c9
quannhhgcc19187 <91667696+quannhhgcc19187@users.noreply.github.com>
2021-11-18 01:28:46 +0700

    Update Add_Product.php

--- Add_Product.php
+++ Add_Product.php
@@ -101,6 +101,12 @@
 							<div class="col-sm-10">
 							      <?php bind_Category_List($conn);  ?>
 							</div>
+                </div> 
+				<div class="form-group">   
+                    <label for="" class="col-sm-2 control-label">Shop(*):  </label>
+							<div class="col-sm-10">
+							      <?php bind_Shop_List($conn);  ?>
+							</div>
                 </div>  
                           
                 <div class="form-group">  
